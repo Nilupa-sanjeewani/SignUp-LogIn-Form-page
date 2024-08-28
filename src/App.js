90279ec05c9ada3b79b3e0c6853ac6d59f048042
nilupa786 <129794165+Nilupa-sanjeewani@users.noreply.github.com>
2024-08-28 11:08:09 +0530

    signUp-Login form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import signup from './signup/signup';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/signup/signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './signup.css';
+
+const signup = () => {
+    return (
+        <div className='addUser'>
+            <h3>Sign Up</h3>
+            <form className='addUserForm'>
+                <div className='inputGroup'></div>
+            </form>
+        </div>
+    )
+}
+
+export default signup
