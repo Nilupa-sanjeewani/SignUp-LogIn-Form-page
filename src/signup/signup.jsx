d41586d60578922f9498c75a0d22c8aefa647565
nilupa786 <129794165+Nilupa-sanjeewani@users.noreply.github.com>
2024-08-30 10:24:53 +0530

    update signin

--- src/signup/signup.jsx
+++ src/signup/signup.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from 'react';
 import './signup.css';
+import { Link } from 'react-router-dom';
+import Login from '../login/Login';
 
-const signup = () => {
+const Signup = () => {
     return (
         <div className='addUser'>
             <h3>Sign Up</h3>
             <form className='addUserForm'>
-                <div className='inputGroup'></div>
+                <div className='inputGroup'>
+                    <label htmlFor='name'>Name:</label>
+                    <input
+                        type='text'
+                        id='name'
+                        autoComplete='off'
+                        placeholder='Enter your name'
+                    />
+                    <label htmlFor='name'>Email:</label>
+                    <input
+                        type='text'
+                        id='name'
+                        autoComplete='off'
+                        placeholder='Enter your email'
+                    />
+                    <label htmlFor='name'>password:</label>
+                    <input
+                        type='password'
+                        id='name'
+                        autoComplete='off'
+                        placeholder='Enter your password'
+                    />
+                    <button type="submit" class="btn btn-success">Sign Up</button>
+                </div>
             </form>
+            <div className='login'>
+                <p>Already have an account? </p>
+                <Link to="/login" type="submit" class="btn btn-primary">
+                    Login</Link>
+            </div>
         </div>
-    )
+    );
 }
 
-export default signup
+export default Signup;
